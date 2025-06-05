0a4028513ceb5677c9c3e1513ba7a2200131bbbf
Sumithra <sumithrahungarcutta@gmail.com>
2025-06-05 11:54:32 +0530

    Second commit

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,12 @@ builder.Services.AddDbContext<DataDBContect>(option =>
 {
     option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+   .WithOrigins("http://localhost:4200","https://localhost:7278"));
 
 app.MapControllers();
 
